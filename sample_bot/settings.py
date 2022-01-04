5bbebefaf9c9a61b9c7165c3a93b75426f6dda8d
Yuki_N <59194511+Yuki0520-ba@users.noreply.github.com>
2022-01-04 16:22:17 +0900

    Update settings.py

--- sample_bot/settings.py
+++ sample_bot/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g)_ub7k60p9@g5^!i1han7b5d-9xgs%-tnkv2s-kw-v-1*9+a)'
+# Input your Secretkey
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
